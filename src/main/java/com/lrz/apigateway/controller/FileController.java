933d06053eee3746e5e7a2d684db59004b656fd7
Lara Rodrigues <lararods73@gmail.com>
2024-07-05 15:01:23 -0300

    feat: adds upload of multiple files at once

--- src/main/java/com/lrz/apigateway/controller/FileController.java
+++ src/main/java/com/lrz/apigateway/controller/FileController.java
@@ -11,10 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.lrz.apigateway.services.FileStorageService;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 /**
  *
  * @author lara
@@ -23,20 +27,27 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @RestController
 @RequestMapping("/api/file/v1")
 public class FileController {
-    
+
     private Logger logger = Logger.getLogger(FileController.class.getName());
-    
+
     @Autowired
     private FileStorageService service;
-    
+
     @PostMapping("/upload")
-    public UploadFileResponseVO uploadFile(@RequestParam("file") MultipartFile file){
+    public UploadFileResponseVO uploadFile(@RequestParam("file") MultipartFile file) {
         logger.info("Storing file to disk");
         var filename = service.storeFile(file);
-        
+
         String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/file/v1/download/")
                 .path(filename).toUriString();
-        
+
         return new UploadFileResponseVO(filename, fileDownloadUri, file.getContentType(), file.getSize());
+    }
+
+    @PostMapping("/uploadMultiple")
+    public List<UploadFileResponseVO> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        logger.info("Storing files to disk");
+
+        return Arrays.asList(files).stream().map(file -> uploadFile(file)).collect(Collectors.toList());
     }
 }
