c3d98494c9f5764bf5fc0845be28f0382cc391c3
Lara Rodrigues <lararods73@gmail.com>
2024-07-06 15:44:05 -0300

    feat: implements file download(by name)

--- src/main/java/com/lrz/apigateway/controller/FileController.java
+++ src/main/java/com/lrz/apigateway/controller/FileController.java
@@ -11,9 +11,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.lrz.apigateway.services.FileStorageService;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -49,5 +56,26 @@ public class FileController {
         logger.info("Storing files to disk");
 
         return Arrays.asList(files).stream().map(file -> uploadFile(file)).collect(Collectors.toList());
+    }
+
+    @GetMapping("/download/{filename:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename, HttpServletRequest request) {
+        logger.info("reading file on disk");
+        Resource resource = service.loadFileAsResource(filename);
+
+        String contentType = "";
+
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (Exception e) {
+            logger.info("Couldnt read file type");
+        }
+
+        if (contentType.isBlank()) {
+            contentType = "application/octet-stream";
+        }
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filname=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
--- src/main/java/com/lrz/apigateway/services/FileStorageService.java
+++ src/main/java/com/lrz/apigateway/services/FileStorageService.java
@@ -6,49 +6,70 @@ package com.lrz.apigateway.services;
 
 import com.lrz.apigateway.config.FileStorageConfig;
 import com.lrz.apigateway.exception.FileStorageException;
+import com.lrz.apigateway.exception.MyFileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+
 /**
  *
  * @author lara
  */
 @Service
 public class FileStorageService {
+
     private final Path fileStorageLocation;
 
     @Autowired
     public FileStorageService(FileStorageConfig fileStorageConfig) {
         Path path = Paths.get(fileStorageConfig.getUploadDir()).toAbsolutePath().normalize();
         this.fileStorageLocation = path;
-        
+
         try {
             Files.createDirectories(this.fileStorageLocation);
-            
+
         } catch (Exception e) {
-            throw new FileStorageException("Could not create directory where uploaded files will be stored",e);
+            throw new FileStorageException("Could not create directory where uploaded files will be stored", e);
         }
     }
-    
-    public String storeFile(MultipartFile file){
+
+    public String storeFile(MultipartFile file) {
         String filename = StringUtils.cleanPath(file.getOriginalFilename());
-        
+
         try {
-            if(filename.contains("..")){
+            if (filename.contains("..")) {
                 throw new FileStorageException("Sorry! Invalid path sequence:" + filename);
             }
             Path targetLocation = this.fileStorageLocation.resolve(filename);
             Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
             return filename;
         } catch (Exception e) {
-            throw new FileStorageException("Could not store file" + filename,e);
+            throw new FileStorageException("Could not store file" + filename, e);
 
         }
     }
-    
+
+    public Resource loadFileAsResource(String filename) {
+        try {
+            Path filepath = this.fileStorageLocation.resolve(filename).normalize();
+
+            Resource resource = new UrlResource(filepath.toUri());
+            if (resource.exists()) {
+                return resource;
+            } else {
+                throw new MyFileNotFoundException("file not found");
+            }
+
+        } catch (Exception e) {
+            throw new MyFileNotFoundException("file not found", e);
+        }
+    }
+
 }
