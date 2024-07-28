e1683eb624ccbf506f2eb756c1de84db3cdee3e3
Lara Rodrigues <lararods73@gmail.com>
2024-07-28 11:07:40 -0300

    refactor: changes file upload dir

--- src/test/java/com/lrz/apigateway/config/TestConfigs.java
+++ src/test/java/com/lrz/apigateway/config/TestConfigs.java
@@ -9,7 +9,7 @@ package com.lrz.apigateway.config;
  * @author lara
  */
 public class TestConfigs {
-    public static final int SERVER_PORT= 8888;
+    public static final int SERVER_PORT= 8888;///mude se for executar com docker
     public static final String HEADER_PARAM_ORIGIN = "Origin";
     public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
     public static final String CONTENT_TYPE_JSON = "application/json";
